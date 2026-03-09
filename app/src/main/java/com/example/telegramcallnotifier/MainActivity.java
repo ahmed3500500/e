09e5fab31889ca442eec578b3ffbd2e633121e60
ahmed3500500 <ahmed3500599@gmail.com>
2026-03-09 14:21:25 -0500

    Fix_Missing_Uri_Import

--- app/src/main/java/com/example/telegramcallnotifier/MainActivity.java
+++ app/src/main/java/com/example/telegramcallnotifier/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
